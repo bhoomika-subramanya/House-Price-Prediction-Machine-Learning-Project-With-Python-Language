5c9e16979d6056dbf7232887a4f88b09b4f264dd
Bhoomika Subramanya <bhoomikasubramanya3172004@gmail.com>
2025-11-23 20:54:33 +0530

    Fixed folder, updated env, working improved model

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\house pp\venv
+set VIRTUAL_ENV=D:\House_Price_Prediction_Using_Machine_Learning\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
